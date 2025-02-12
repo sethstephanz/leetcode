99d8975325caa95d2bc33b09b2e0c038ed01cd73
Seth Stephanz <sethstephanz@gmail.com>
2025-02-12 10:57:57 -0500

    Rename binary.py to bit_shifting.py. Add drills and bin_search.py

---
+++ to_drill.py
@@ -0,0 +1,13 @@
+def bin_search(nums, target):
+    l, r = 0, len(nums) - 1
+
+    while l <= r:
+        m = l + (r - l) // 2
+        if nums[m] == target:
+            return m
+        elif nums[m] > target:
+            r = m - 1
+        else:
+            l = m + 1
+
+    return -1  # not found
